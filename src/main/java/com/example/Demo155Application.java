d32d90d83613800aba727b0d171d8e1a66e6dae2
hseitsmstudent <hse.itsm.student@gmail.com>
2017-05-11 14:19:06 +0300

    Initial commit

---
+++ src/main/java/com/example/Demo155Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo155Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo155Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/Person.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by student on 4/6/17.
+ */
+
+@Data
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue
+
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+}
